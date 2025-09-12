ec53171f7e7b487549d470410511cc52f9d1ec4f
Pavel Kaspiarovich <p.kaspiarovich@andersenlab.com>
2025-09-12 18:05:24 +0200

    false

--- workspaces/repos/main.tf
+++ workspaces/repos/main.tf
@@ -19,7 +19,7 @@ locals {
     test_api = {
       type     = "backend"
       language = "python"
-      has_page = false
+      has_page = true
     }
     test_web = {
       type     = "frontend"
