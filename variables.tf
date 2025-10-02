bc6df2b5462cd93b131d28b4581f152e907f6397
Pavel Kaspiarovich <p.kaspiarovich@andersenlab.com>
2025-10-02 23:25:50 +0200

    BUG 1

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 # Input variables for the root module
 
 variable "project_name" {
-  description = "Name of the project"
+  description = "Name of the project BUG 1"
   type        = string
   default     = "terraform-demo"
 }
