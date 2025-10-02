d2f62192e4c81dfb348b115b6716bb5ea48a109e
Pavel Kaspiarovich <p.kaspiarovich@andersenlab.com>
2025-10-02 18:09:41 +0200

    change by developer 1

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "project_name" {
 variable "author" {
   description = "Author name for the info page"
   type        = string
-  default     = "Elvis Little"
+  default     = "Elvis Little - changed by developer 1"
 }
 
 variable "github_token" {
